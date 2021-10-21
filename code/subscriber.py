85f0ccdea122b9a30a76cf599108fee5d21231e1
Bartosz Piech <piech.bartosz696@gmail.com>
2021-10-21 19:09:21 +0200

    added pub-sub connection

---
+++ code/subscriber.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import zmq
+import signal
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+socket.connect('tcp://localhost:5555')
+socket.setsockopt(zmq.SUBSCRIBE, b'status')
+
+while (True):
+    message = socket.recv_multipart()
+    print(message)
