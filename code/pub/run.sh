263c672fa007c8c07fb2e7013f4a2e2bc9d0c192
Bartosz Piech <piech.bartosz696@gmail.com>
2021-12-22 19:10:02 +0100

    cleaned up some code, moved stuff into separate files and implemented invocation counter for flask api calls, but does not work for now

--- code/pub/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# not needed for now
-
-for i in {1..10}
-do
-	docker run --net="host" publisher
-done
--- code/run_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export FLASK_APP="blockchain"       # program name
-export FLASK_ENV="development"      # development -- allows to debug more easily
-flask run                           # run app using flask
